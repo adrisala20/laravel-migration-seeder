8fa26ebf7fac33dbb9b085750e5bf61eaa309d5f
adrisala20 <adriana_15_tuya@hotmail.com>
2024-05-22 17:21:02 +0200

    added columns

--- database/migrations/2024_05_22_133457_create_trains_table.php
+++ database/migrations/2024_05_22_133457_create_trains_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 255);
+            $table->string('stazione_di_partenza',255);
+            $table->string('stazione_di_arrivo',255);
+            $table->timeTz('orario_di_partenza', $precision = 0);
+            $table->timeTz('orario_di_arrivo', $precision = 0);
+            $table->string('codice_treno',5);
+            $table->smallInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+            $table->string('tipo_di_treno',255);
+            $table->string('disponibilità_posti',255);
             $table->timestamps();
         });
     }
